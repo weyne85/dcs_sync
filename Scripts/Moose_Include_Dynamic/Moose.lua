2219a157b556948d8067728d13c920e728f5a918
weyne85 <weyne.becker@gmail.com>
2026-02-28 15:57:32 +0100

    Aufräumarbeiten

---
+++ Scripts/Moose_Include_Dynamic/Moose.lua
@@ -0,0 +1,26 @@
+env.info( '*** MOOSE DYNAMIC INCLUDE START *** ' )
+
+local base = _G
+
+__Moose = {}
+
+__Moose.Include = function( IncludeFile )
+	if not __Moose.Includes[ IncludeFile ] then
+		__Moose.Includes[IncludeFile] = IncludeFile
+		local f = assert( base.loadfile( IncludeFile ) )
+		if f == nil then
+			error ("Moose: Could not load Moose file " .. IncludeFile )
+		else
+			env.info( "Moose: " .. IncludeFile .. " dynamically loaded." )
+			return f()
+		end
+	end
+end
+
+__Moose.Includes = {}
+if not MOOSE_DEVELOPMENT_FOLDER then
+	MOOSE_DEVELOPMENT_FOLDER='Scripts'
+end
+__Moose.Include( MOOSE_DEVELOPMENT_FOLDER..'/Moose/Modules.lua' )
+BASE:TraceOnOff( true )
+env.info( '*** MOOSE INCLUDE END *** ' )
